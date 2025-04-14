ebf0076462ddb79af8a252a4e879b026282d66fe
GarishmaChug <garishma3016.beai24@chitkara.edu.in>
2025-04-14 23:19:10 +0530

    added database

--- Project1/Evaluation2/djangoApp/urls.py
+++ Project1/Evaluation2/djangoApp/urls.py
@@ -1,45 +1,25 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('' , views.home_view,name='home'),
-
-    path('all' , views.all_view,name='all'),
-    
-
-    path('beauty', views.beauty_view, name='beauty'),
-    # path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-
-    # path('all' , views.all_view,name='all'),
-    path('snacks' , views.snacks_view,name='snacks'),
-    # path('pharmacy' , views.pharmacy_view,name='pharmacy'),
-
-
     path('register/',views.register_view,name='register'),
     path('login/',views.login_view,name='login'),
     path('dashboard/',views.dashboard_view,name='dashboard'),
     path('logout/',views.logout_view,name='logout'),
     # path('profile/',views.product_view,name='profile'),
     # path('product/',views.product_view,name='product'),
-
-
+    path('search', views.search, name='search'),
+    path('all', views.all_view, name='all'),
     path('pharmacy', views.pharmacy_view, name='pharmacy'),
-    # path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-
-    # path('fruits' , views.fruits_view,name='fruits'),
-    
     path('fruits', views.fruits_view, name='fruits'),
-    # path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('beauty', views.beauty_view, name='beauty'),
+    path('snacks', views.snacks_view, name='snacks'),
+
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
 
-
-
-    path('all', views.all_view, name='all_products'),
-#     path('add-to-cart/<int:product_id>', views.add_to_cart, name='add_to_cart'),
-
-
-# path('cart/', views.cart_detail, name='cart_detail'),
-#     path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
-
-]
+   
